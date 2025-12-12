9fce38a4715d0f528c1890c9ad70ac37774c663d
EthanWonCho <realethanwoncho@gmail.com>
2025-12-13 08:04:13 +0900

    add more error messages

--- app/src/middleware/error-handler.js
+++ app/src/middleware/error-handler.js
@@ -3,7 +3,12 @@ import env from '../config/env.js';
 const clientErrorMessage = {
   404: "We couldn't find the page you were looking for.",
   418: 'We have an unexpected error. (I became a teapot)',
+  400: 'The request was invalid. Please check and try again.',
+  401: 'You need to sign in to continue.',
+  403: "You don't have permission to do that.",
   500: 'We have an internal server error.',
+  502: 'Bad gateway. Please try again later.',
+  503: 'Service temporarily unavailable.',
 };
 
 function errorHandler(err, req, res, next) {
